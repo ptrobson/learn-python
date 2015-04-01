bd2f3e9a556cefd590ffe30da61814d1bf9ecdf5
acharlton <a.charlton32@gmail.com>
2015-04-01 23:13:16 +0100

    Added string formatting problems

---
+++ string-formatting.py
@@ -0,0 +1,38 @@
+"""
+String Formatting Problems
+==========================
+
+This file contains a set of functions which need to be completed so that they
+perform the task outlined in the docstring.
+
+Each docstring contains examples of what your code should do, which look like:
+
+    >> some_function(some_value)
+    some_output
+
+These are used in the testing functions, don't change them!
+
+To test all your code when you've finished - just call run_tests()
+"""
+
+import doctest
+def run_tests():
+    """This function just tests all the code - don't make any changes!"""
+    print('Testing all functions: ')
+    doctest.testmod()
+
+
+
+def bottles_of_beer(n):
+    """
+    Produce the string "n bottles of beer on the wall, n bottles of beer!"
+    >>> bottles_of_beer(10)
+    '10 bottles of beer on the wall, 10 bottles of beer!'
+    >>> bottles_of_beer(5)
+    '5 bottles of beer on the wall, 5 bottles of beer!'
+    """
+
+    return ''
+
+
+
