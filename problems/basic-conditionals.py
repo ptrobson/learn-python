47ef38468acac3db43473d938084fb37901f4a83
Andrew Charlton <a.charlton32@gmail.com>
2015-04-19 21:10:52 +0100

    Added basic problem sets

---
+++ problems/basic-conditionals.py
@@ -0,0 +1,30 @@
+
+
+"""
+Basic Problems involving Conditionals
+=====================================
+
+This file contains a set of functions which need to be completed so that they
+perform the task outlined in the docstring.
+
+Each docstring contains examples of what your code should do, which look like:
+
+    >> some_function(some_value)
+    some_output
+
+These are used in the testing functions, don't change them!
+
+To test all your code when you've finished - just call run_examples()
+"""
+
+# ======================= TESTING - IGNORE ===================================
+import doctest
+def run_examples():
+    """This function just tests all the code - don't make any changes!"""
+    print('Testing examples:')
+    doctest.testmod()
+
+
+# ========================== Start here ======================================
+
+
---
+++ tutorials.md
@@ -0,0 +1,53 @@
+Python Tutorials
+================
+
+Easy
+====
+
+
+Introduction - 2010/01
+----------------------
+
+* Introduction
+* Variables
+* Printing
+* User input
+
+
+Functions - 2010/02
+-------------------
+
+* Defining functions x
+
+
+Conditionals - 2010/03
+----------------------
+
+* If/elif/else
+* And/Or
+* Nesting conditionals
+
+* Conditionals with integers
+* Conditionals with strings
+
+
+Iteration - 2010/04
+-------------------
+
+* For loops
+* Advanced range
+* Counting/summing
+* Iterating through strings
+* Iterating through lists
+
+
+Intermediate
+============
+
+
+
+
+Hard
+====
+
+
