1f23f01e3668203e9ce7b989d0e59c8c9155f28b
acharlton <a.charlton32@gmail.com>
2015-04-20 12:14:22 +0100

    Added for loop problem sets

--- problems/basic-loops.py
+++ problems/basic-loops.py
@@ -1,5 +1,3 @@
-
-
 """
 Basic Problems involving For Loops
 ==================================
@@ -28,4 +26,61 @@ def run_examples():
 # ========================== Start here ======================================
 
 
-def 
+def seven_times(a, b):
+    """
+    PRINT the seven times table, for multiples between 7*a and 7*b.
+    The times table should be printed in the same format as seen in the
+    tutorial notes.
+    >>> seven_times(3, 5)
+    3 x 7 = 21
+    4 x 7 = 28
+    5 x 7 = 35
+    >>> seven_times(99, 100)
+    99 x 7 = 693
+    100 x 7 = 700
+    """
+
+    return
+
+
+def fizzbuzz_sum(n):
+    """
+    Find the sum of all the positive integers less than n that are either
+    a multiple of 3 or 5 or both.
+    >>> fizzbuzz_sum(20)
+    78
+    >>> fizzbuzz_sum(100)
+    4635
+    """
+
+    return
+
+
+def arithmetic_sum(a, d, n):
+    """
+    An arithmetic sequence starts with the number a, goes up each time by d,
+    and has n terms, e.g. if a = 3, d = 2, n = 7 the sequence will be:
+    3, 5, 7, 9, 11, 13, 15
+    Find the sum of the sequence
+    >>> arithmetic_sum(3, 5, 20)
+    1010
+    >>> arithmetic_sum(14, 23, 80)
+    73800
+    """
+
+    return
+
+
+def factorial(n):
+    """
+    The factorial of a number is calculated by multiplying all the
+    positive integers smaller than that number together, e.g.
+    5! (factorial) = 5 x 4 x 3 x 2 x 1.
+    Return the factorial of n (assume n is a positive integer).
+    >>> factorial(5)
+    120
+    >>> factorial(12)
+    479001600
+    """
+
+    return
---
+++ problems/loops-with-strings-and-lists.py
@@ -0,0 +1,124 @@
+"""
+Looping with Strings and Lists
+==============================
+
+This file contains a set of functions which need to be completed so that they
+perform the task outlined in the docstring.
+
+Each docstring contains examples of what your code should do, which look like:
+
+    >> some_function(some_value)
+    some_output
+
+These are used in the testing functions, don't change them!
+
+To test all your code when you've finished - just call run_examples()
+"""
+
+# ======================= TESTING - IGNORE ===================================
+import doctest
+def run_examples():
+    """This function just tests all the code - don't make any changes!"""
+    print('Testing examples:')
+    doctest.testmod()
+
+
+# ========================== Start here ======================================
+
+
+def e_words(words):
+    """
+    Given a list of words, count the number that contain an 'e' or 'E'.
+    >>> e_words(['mary', 'had', 'a', 'little', 'lamb'])
+    1
+    >>> e_words(['Peter', 'Piper', 'picked', 'a', 'peck', 'of', 'pepper'])
+    5
+    """
+
+    return
+
+
+def short_and_sweet(words, length):
+    """
+    Given a list of words, create a string (seperated by spaces) of all the
+    words with a size less than length.
+    >>> short_and_sweet(['A', 'hugely', 'big', 'orange', 'elephant', 'pet'], 4)
+    'A big pet'
+    >>> short_and_sweet(['Go', 'down', 'to', 'this', 'dark', 'sea'], 4)
+    'Go to sea'
+    """
+
+    return
+
+
+def list_average(numbers):
+    """
+    Given a list of numbers, return their average (mean), rounded down to the
+    nearest integer.
+    >>> list_average(4, 7, 9)
+    6
+    >>> list_average(3, 3, 5, 10)
+    5
+    """
+
+    return
+
+
+def letter_location(string, letter):
+    """
+    Given a string, find all the occurences of 'letter' and add up the
+    index values of the places where they occur.  Return this sum.
+    >>> letter_location('hello billy', 'l')
+    22
+    >>> letter_location('why oh why', 'y')
+    11
+    """
+
+    return
+
+
+def weighted_sum(numbers):
+    """
+    A list is used to keep track of scores students achieve in tests and
+    homeworks.  The scores alternate between homework and assessment results,
+    with the first score being a homework.
+    Assessment scores are worth double homework scores.
+    Every 3rd assessment is a mock exam, worth triple points.
+    Find the weighted sum of the scores, ie. Homeworks + 2 x Tests + 3 x Mocks
+    >>> weighted_sum([5, 8, 7])
+    28
+    >>> weighted_sum([3, 4, 2, 5, 8, 6, 9])
+    58
+    """
+
+    return
+
+
+def html_parse(string):
+    """
+    HTML uses tags such as <b> to apply formatting, hyperlinks etc. to content
+    on websites.  Given a string of HTML, return an output string stripped of
+    all the tags.  Anything enclosed between '<' and '>' should be discarded.
+    >>> html_parse("This is some <b>bold text</b>, and <i>italics!</i>")
+    "This is some bold text, and italics!
+    >>> html_parse("<a href=www.google.com>Link to <b>GOOGLE</b></a>")
+    "Link to GOOGLE"
+    """
+
+    return
+
+
+def odd_letters(words):
+    """
+    An *odd letter* in a word is defined as one where the index of that letter
+    is odd, e.g. in 'abcde', 'b' and 'd' are the odd letters.
+    Similarly, an *odd word* in a list is one where the index of that word
+    is odd.
+    Create a string composed of all the odd letters in the odd words.
+    >>> odd_letters(['This', 'is', 'quite', 'hard'])
+    'hssutad'
+    >>> odd_letters(['Do', 'we', 'need', 'some', 'nesting?'])
+    'oeedoeetn?'
+    """
+
+    return
